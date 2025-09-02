f2c895bbc49a30116830aecbadd421c1db06c0a9
Adam Fritz <62766637+hammahutz@users.noreply.github.com>
2025-09-02 17:11:42 +0200

    Tutorial/05 content pipeline (#2)
    * Fix: Change the content Directory
    
    * finnish tutorial 05

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -11,22 +11,24 @@ namespace DungeonSlime;
 public class Game1 : Core
 {
     private SpriteFont _font;
+    private Texture2D _logo;
+    private Rectangle iconSourceRect = new(0, 0, 128, 128);
+    private Rectangle wordmarkSourceRect = new(150, 34, 458, 58);
+
     public Game1()
-        : base("DungeonSlime") { }
+        : base("DungeonSlime", width: 1920, height: 1080) { }
 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        Console.WriteLine("Hello");
 
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-
-        _font = Content.Load<SpriteFont>("font");
-
+        _font = Content.Load<SpriteFont>("Fonts/Font");
+        _logo = Content.Load<Texture2D>("Images/Logo");
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,9 +50,11 @@ public class Game1 : Core
 
         SpriteBatch.Begin();
         SpriteBatch.DrawString(_font, "Hello World", new Vector2(100, 100), Color.White);
-        #if OPENGL
+#if OPENGL
         SpriteBatch.DrawString(_font, "OpenGL", new Vector2(132, 135), Color.Blue);
-        #endif
+#endif
+
+        SpriteBatch.Draw(_logo, new Vector2(Window.ClientBounds.Width, Window.ClientBounds.Height) / 2, iconSourceRect, Color.Red * 0.5f, 0, new Vector2(_logo.Width, _logo.Height) / 2, 1.0f, SpriteEffects.None, 0.0f);
         SpriteBatch.End();
 
         base.Draw(gameTime);
