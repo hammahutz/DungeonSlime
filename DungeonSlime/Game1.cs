f74175c310598d02bb57d9fbb2bb0251131ddaac
hammahutz <62766637+hammahutz@users.noreply.github.com>
2025-09-02 16:05:43 +0200

    finnish tutorial 05

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+
 using DungeonSlime.Library;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,14 +12,15 @@ public class Game1 : Core
 {
     private SpriteFont _font;
     private Texture2D _logo;
+    private Rectangle iconSourceRect = new(0, 0, 128, 128);
+    private Rectangle wordmarkSourceRect = new(150, 34, 458, 58);
 
     public Game1()
-        : base("DungeonSlime") { }
+        : base("DungeonSlime", width: 1920, height: 1080) { }
 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        Console.WriteLine("Hello");
 
         base.Initialize();
     }
@@ -51,7 +54,7 @@ public class Game1 : Core
         SpriteBatch.DrawString(_font, "OpenGL", new Vector2(132, 135), Color.Blue);
 #endif
 
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+        SpriteBatch.Draw(_logo, new Vector2(Window.ClientBounds.Width, Window.ClientBounds.Height) / 2, iconSourceRect, Color.Red * 0.5f, 0, new Vector2(_logo.Width, _logo.Height) / 2, 1.0f, SpriteEffects.None, 0.0f);
         SpriteBatch.End();
 
         base.Draw(gameTime);
